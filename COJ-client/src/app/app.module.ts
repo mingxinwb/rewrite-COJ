870cb0f02dd8f79269d6cb9ddfbb54863ad32ff3
mingxinwb <mingxinwb@gmail.com>
2018-07-04 08:40:19 -0700

    3rd component: new-problem

--- COJ-client/src/app/app.module.ts
+++ COJ-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { DataService } from './services/data.service';
 import { routing } from './app.routes';
@@ -7,16 +8,19 @@ import { routing } from './app.routes';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [
     DataService
